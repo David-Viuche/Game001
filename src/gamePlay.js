fffb938af2c56f2c09c095c5bcf4b33006a5a1d3
David Viuche <jviucheroja@uniminuto.edu.co>
2021-11-08 21:13:18 -0500

    Se carga imagen de fondo y se cambia metodo de renderizado a canvas

--- src/gamePlay.js
+++ src/gamePlay.js
@@ -6,11 +6,13 @@ GamePlayManager = {
     },
     //carga recursos del proyecto
     preload: function () {
-        console.log('preload');
+        //se carga imagen de fondo
+        game.load.image('background', 'assets/images/background.png');
     },
     //una vez cargue recursos llama create
     create: function () {
-        console.log('create');
+        //agregar sprite (coordenadas, id imagen)
+        game.add.sprite(0, 0, 'background');
     },
     //frame a frame lo llama
     update: function () {
@@ -22,10 +24,10 @@ GamePlayManager = {
 // Phaser.CANVAS por defecto
 // Phaser.WEBGL usa la tarjeta grafica 
 // Phaser.AUTO intenta usar WEBGL si no encuentra tarjeta grafica usa CANVAS
-let game = new Phaser.Game(1136, 640, Phaser.AUTO);
+let game = new Phaser.Game(1136, 640, Phaser.CANVAS);
 
 //se asgina estado al objeto GamePlayManager con el id gameplay
 game.state.add('gameplay', GamePlayManager);
 
 //inicia el state con id gameplay
-game.state.start('gameplay')
+game.state.start('gameplay');
